0b757f23f1d37c7997fd325932325cd39659ac29
Samuel Hunter <lawful.lazy@gmail.com>
2018-02-03 11:48:10 -0800

    Add SS_CubeManipulator and RobotMap mockup channels

--- src/org/usfirst/frc/team3663/robot/RobotMap.java
+++ src/org/usfirst/frc/team3663/robot/RobotMap.java
@@ -4,23 +4,25 @@ package org.usfirst.frc.team3663.robot;
  * If the motor number is 19 that means that it will not be assigned to any thing
  */
 public class RobotMap {
-
-
+	// Digital Inputs
 	public static final int LIMIT_SWITCH = 0;
+	public static final int LIMIT_SWITCH_INTAKE = -1;
 	
+	// Talons
 	public static final int DRIVE_LEFT = 1;
 	public static final int DRIVE_RIGHT = 3;
+	public static final int LIFT = 2;
+	public static final int ELEVATOR = -1;
 	
-	public static int LIFT = 2;
-
-	public static final int CUBE_INTAKE = -1;
-	public static final int CUBE_HOLDER = -1;
+	public static final int CUBE_INTAKE_LEFT = -1;
+	public static final int CUBE_INTAKE_RIGHT = -1;
+	public static final int CUBE_SHOOTER = -1;
 	public static final int CUBE_ROTATOR = -1;
 	
-	public static final int ELEVATOR = -1;
-
-	
-/***CLIMBER PORTS***/
-	//motor
+	// Pneumatics
+	public static final int CUBE_SHOOTER_FORWARD = -1;
+	public static final int CUBE_SHOOTER_REVERSE = -1;
+	public static final int CUBE_INTAKE_FORWARD = -1;
+	public static final int CUBE_INTAKE_REVERSE = -1;
 	
 }
