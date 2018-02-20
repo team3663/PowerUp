16e00ac4e08458730bfa9e47c50e8fc0a6d40647
Samuel Hunter <lawful.lazy@gmail.com>
2018-02-19 18:56:47 -0800

    Update RobotMap

--- src/org/usfirst/frc/team3663/robot/RobotMap.java
+++ src/org/usfirst/frc/team3663/robot/RobotMap.java
@@ -5,40 +5,54 @@ package org.usfirst.frc.team3663.robot;
  * thing
  */
 public class RobotMap {
-	// Digital Inputs
-	public static final int LIMIT_SWITCH_ELEVATOR_TOP = 1;
+	
+	/// You expected analog ports, but it's me!-- DIO!
+	public static final int LIMIT_SWITCH_ELEVATOR_TOP = 0;
 	public static final int LIMIT_SWITCH_ELEVATOR_BOTTOM = -1;
-	public static final int LIMIT_SWITCH_INTAKE = 0;
-	public static final int CUBE_ROTATOR_SENSOR = -1;
+	public static final int LIMIT_SWITCH_INTAKE = 1;
+	
+	
+	// Encoders
+	public static final int DRIVE_RIGHT_ENC_1 = 4;
+	public static final int DRIVE_RIGHT_ENC_2 = 5;
+	public static final int DRIVE_LEFT_ENC_1 = 6;
+	public static final int DRIVE_LEFT_ENC_2 = 7;
+	public static final int ELEVATOR_ENC_1 = 9;
+	public static final int ELEVATOR_ENC_2 = 10;
 
+	
+	/// Analog
+	public static final int CUBE_ROTATOR_SENSOR = 0; // Potentiometer
+
+	
+	/// CAN BUS
 	// Talons
-	public static final int DRIVE_LEFT_1 = 11;
-	public static final int DRIVE_LEFT_2 = 12;
-	public static final int DRIVE_LEFT_3 = 13;
+	public static final int CLIMBER_1 = 1;
+	public static final int CLIMBER_2 = 4;
+	
+	public static final int ELEVATOR_1 = 2;
+	public static final int ELEVATOR_2 = 3;
 	
 	public static final int DRIVE_RIGHT_1 = 8;
 	public static final int DRIVE_RIGHT_2 = 9;
 	public static final int DRIVE_RIGHT_3 = 10;
-
-	public static final int ELEVATOR_1 = 2;
-	public static final int ELEVATOR_2 = 3;
+	
+	public static final int DRIVE_LEFT_1 = 11;
+	public static final int DRIVE_LEFT_2 = 12;
+	public static final int DRIVE_LEFT_3 = 13;
 
 	public static final int CUBE_INTAKE_LEFT = 51;
 	public static final int CUBE_INTAKE_RIGHT = 43;
 	
 	public static final int CUBE_SHOOTER = 54;
 	public static final int CUBE_ROTATOR = 53;
-	
-	public static final int CLIMBER_1 = 1;
-	public static final int CLIMBER_2 = 4;
-	
 
 	// Pneumatics
-	public static final int CLIMBER_PNEUM_FWD = 3; // TODO: figure out this order
-	public static final int CLIMBER_PNEUM_REV = 2;
-	
 	public static final int GRIFF_SQUEEZE_FWD = 1;
 	public static final int GRIFF_SQUEEZE_REV = 0;
+	
+	public static final int CLIMBER_PNEUM_FWD = 3;
+	public static final int CLIMBER_PNEUM_REV = 2;
 
 	public static final int CUBE_INTAKE_LIFT_FOWARD = 7;
 	public static final int CUBE_INTAKE_LIFT_REVERSE = 6;
