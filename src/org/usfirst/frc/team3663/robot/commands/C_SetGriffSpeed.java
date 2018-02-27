0af16965cdc13b87799c40ce643968d4afa34e9f
RandomPotato26 <random.potato26@gmail.com>
2018-02-26 18:56:20 -0800

    nvm that was a mistake

---
+++ src/org/usfirst/frc/team3663/robot/commands/C_SetGriffSpeed.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team3663.robot.commands;
+
+import org.usfirst.frc.team3663.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class C_SetGriffSpeed extends Command {
+	private final double spd;
+
+	public C_SetGriffSpeed(double spd) {
+		requires(Robot.ss_griff);
+		this.spd = spd;
+	}
+
+	@Override
+	protected void execute() {
+		Robot.ss_griff.setGriffSpd(spd);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return true; // Runs only once
+	}
+}
